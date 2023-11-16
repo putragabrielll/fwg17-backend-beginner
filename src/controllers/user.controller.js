421a1e3e9fc123c3a9fb817f42d2ffec662c6bfe
putragabrielll <putragmahk@gmail.com>
2023-11-16 17:00:12 +0700

    fix : express

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
-const users = [
+// variable users untuk data awal dari semua users yang ada.
+let users = [
     {
         id: 1,
         name: "Gabriel Putra Sihombing",
@@ -16,32 +17,52 @@ const users = [
     }
 ]
 
-exports.getAllUsers = (req, res) => {
+let countIdUser = users.length // akan mencari berapa users.length terakhir.
+
+// memanggil semua users
+exports.getAllUsers = (req, res) => { 
     return res.json({
         success: true,
         message: "List all users",
-        result: users
+        result: users // akan memanggil semua data yg dimana sebagai diambil dari variabel users
     });
+};
+
+// memanggil semua user berdasarkan Id nya
+exports.getUsersId = (req, res) => {
+    console.log(req.params.id) // untuk debug melihat output dari req di terminal.
+    const userId = users.filter(item => item.id === Number(req.params.id)) // untuk mencari data pada variabel users berdasarkan key id yg di input, tetapi akan mengembalikan array of object.
+    if(userId[0]){ // akan melakukan pengecekan apabila userId[0] berisi atau bernilai truthy makan akan menjalankan isi dari if.
+        return res.json({ // akan mereturn object dengan key success, message, dan result, dengan isi userId
+            success: true,
+            message: "Detail users",
+            result: userId[0] // karena yg di dapat data berupa array of object dari userId maka kita bisa tambahkan index ke [0], tujuannya agar yg dihasilkan jadi object saja.
+        });
+    } else { // apabila bernilai fasly makan akan menjalankan isi dari else.
+        return res.status(404).json({ // akan memberikan status 404 dengan json.
+            success: false,
+            message: "User not found"
+        })
+    }
 };
 
 
 exports.createUsers = (req, res) => {
-    return res.json({
+    const {name, email} = req.body // akan menerima inputan dari req.body, dimana yg di input hanya name & email.
+    countIdUser = countIdUser + 1 // akan ditambah 1, dari countIdUser yg ada di line 20.
+
+    const userNew = { // membuat variable baru untuk menampung semua req.body yg sudah dikirimkan di line 51.
+        id: countIdUser, // memanggil dari line 52 tapi sudah di tambah 1.
+        name: name, // mangambil key value "name" dari req.body di line 51.
+        email: email // mengambil key value "email" dari req.body di line 51.
+    }
+
+    users.push(userNew) // variable baru yg bernama userNew yg menerima input yg diperoleh dari req.body akan di push ke variable users di line 2.
+    return res.json({ // akan mengembalikan respons json dengan isi nya ada key success, message, dan result, yg dimana result nya berisi variable userNew dari data yg sudah di input di postman.
         success: true,
-        result: [
-            {
-                id: 1,
-                name: "Gabriel Putra Sihombing",
-                email: "puragmahk@gmail.com"
-            },
-            {
-                id: 2,
-                name: "Handoyo Prakarsa",
-                email: "handoyo@gmail.com"
-            },
-            req.body
-        ]
-    });
+        message: "Success add new user!",
+        result: userNew
+    })
 }
 
 exports.updateUsers = (req, res) => {
--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -2,8 +2,10 @@ const allUsers = require('express').Router()
 
 const userController = require('../controllers/user.controller')
 
-allUsers.get('/', userController.getAllUsers)
-allUsers.post('/', userController.createUsers)
+allUsers.get('/', userController.getAllUsers) // akan mengambil data semua user.
+allUsers.get("/:id", userController.getUsersId); // akan mengambil data /users dengan menggunakan id.
+allUsers.post('/', userController.createUsers) // akan melakukan penambahan data baru.
+
 allUsers.delete('/', userController.deleteUsers)
 allUsers.patch('/', userController.updateUsers);
 
