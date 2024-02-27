2c369ea4312f971e299d9f5d8a0e95298c6696d0
putragabrielll <putragmahk@gmail.com>
2024-02-27 23:18:59 +0700

    fix: unit test users controller

--- test/users-controllers.spec.js
+++ test/users-controllers.spec.js
@@ -2,16 +2,19 @@ const { expect } = require("chai")
 const { describe } = require("mocha")
 const userController = require("../src/controllers/admin/user.controller")
 
-describe('User Controller', () => {
+describe('User All Data', () => {
+    const req = {
+        query: {}
+    }
     const res = {
-        JSON: (x) => {
+        json: (x) => {
             return x
         }
     }
 
     it('should return object', async () => {
-        const response = await userController.getAllUsers(null, res)
-        console.log(response)
+        const response = await userController.getAllUsers(req, res)
+        console.log(typeof response)
         expect(typeof response).to.be.equal('object')
     })
 })
