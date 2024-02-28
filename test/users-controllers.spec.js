3bbf4e126e765ff9ba28930fc3210688eed7d4eb
putragabrielll <putragmahk@gmail.com>
2024-02-28 11:33:22 +0700

    fix: test user controller

--- test/users-controllers.spec.js
+++ test/users-controllers.spec.js
@@ -6,6 +6,9 @@ const req = {
     query: {}
 }
 const res = {
+    status: ()=> {
+        return res
+    },
     json: (x) => {
         return x
     }
@@ -18,7 +21,7 @@ describe('User All Data', () => {
     })
     it('should return success', async () => {
         const response = await userController.getAllUsers(req, res)
-        expect(response.success).to.be.equal(false)
+        expect(response.success).to.be.equal(true)
     })
     it('should return pageInfo type: object', async () => {
         const response = await userController.getAllUsers(req, res)
@@ -27,6 +30,25 @@ describe('User All Data', () => {
     it('should return results type: object', async () => {
         const response = await userController.getAllUsers(req, res)
         expect(typeof response.results).to.be.equal('object')
+    })
+    it('should have page 2 in pageInfo if page set to 2', async () => {
+        const req = {
+            query: {
+                page: 2
+            }
+        }
+        const response = await userController.getAllUsers(req, res)
+        expect(response.pageInfo.currentPage).to.be.eq(2)
+    })
+    it('should error when search is not returning any data', async () => {
+        const req = {
+            query: {
+                filter: "random string"
+            }
+        }
+        const response = await userController.getAllUsers(req, res)
+        expect(response.success).to.be.false
+        expect(response.message).to.be.eq('Data not found')
     })
 })
 
