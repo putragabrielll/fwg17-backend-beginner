6bd22bde6e63714a561e6c30de92b6309505bc70
putragabrielll <putragmahk@gmail.com>
2024-03-21 18:36:50 +0700

    fix: update db.lib

--- src/lib/db.lib.js
+++ src/lib/db.lib.js
@@ -1,7 +1,14 @@
 const {Pool} = require('pg')
 
-const db = new Pool ({
-    connectionString: process.env.DATABASE_URL
+const db = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+    
+    // Koneksi lama
+    // connectionString: process.env.DATABASE_URL
 })
 
 // hanya cek apakah koneksi berhasil
